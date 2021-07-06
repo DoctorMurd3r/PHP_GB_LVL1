aba2efe823e40312f4e6fc1c4de17ca96f0cd0e8
DoctorMurd3r <75212227+DoctorMurd3r@users.noreply.github.com>
2021-07-06 10:53:30 +0500

    Update fullimg.php

--- localhost/fullimg.php
+++ localhost/fullimg.php
@@ -2,11 +2,12 @@
 $ID = $_GET['id'];
 $linkDB = mysqli_connect("localhost", "root", "root", "db_images");
 $result = mysqli_query($linkDB, "SELECT * FROM images WHERE id = $ID");     
-     
+$addView = mysqli_query($linkDB, "UPDATE images SET image_views = image_views+1 WHERE id = $ID");
+
 while($row = mysqli_fetch_assoc($result)){
     $img[] = $row;
 }
 echo "<img src={$img['0']['image_src']}><br>";
-
+echo "Число просмотров " . ($img['0']['image_views']+1) . "<br>";
 echo "<a href='5.php'>Вернуться</a>";
 ?>
